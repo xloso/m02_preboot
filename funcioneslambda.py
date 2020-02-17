a97d896cf42718ed5c390f6ddc13ddba8cac228e
xloso <xlosox@gmail.com>
2020-02-17 20:54:34 +0100

    actualizacion ficheros LAMBDAS

--- funcioneslambda.py
+++ funcioneslambda.py
@@ -1,5 +1,11 @@
 from funcionesprimernivel import sumaTodos
 
+doble = lambda x: x*2
+triple = lambda x: x*3
 #lambda son funciones sin nombre
 
-print (sumaTodos(3, lambda x: x**3)
+#print (sumaTodos(3, lambda x: x**3))
+#print(sumaTodos(3, lambda x: x*2))
+
+print(sumaTodos(3, doble))
+print(sumaTodos(100, triple))
--- funcionesprimernivel.py
+++ funcionesprimernivel.py
@@ -1,6 +1,12 @@
 def normal(x):
     return x
 
+normal = lambda x: x # igual a la funcion normal anterior.
+
+# si la funcion va a ser utilizable usar funcion
+# si solo la usas una vez en un proceso te
+#defines una funcion anonima "lambda"
+
 def cuadrado (y):
     return y * y
 
@@ -12,5 +18,10 @@ def sumaTodos(limitTo, f):
     
     return resultado
 
-print (sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__': #solo lo va a ejeecutar el print sin es "principal"
+    #así no lo ejecuta siempre si llamo a esta funcion desde otro sitio
+    
+    print (sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
+else:
+    print (__name__)
