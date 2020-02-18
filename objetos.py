5624aa5566bc7caeb8c11ecd92515d66bfcbfb22
xloso <xlosox@gmail.com>
2020-02-18 01:22:28 +0100

    otra actualizacion tortuga

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+#creamos un objeto de tipo Turtle
+otraTortuguita = turtle.Turtle()
+
+tortuguita.fd(50)
+
+otraTortuguita.color('green')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
