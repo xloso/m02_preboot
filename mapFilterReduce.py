964e4032f2286ceb627f164c5ea76528931c2a49
xloso <xlosox@gmail.com>
2020-02-18 14:55:50 +0100

    map reduce con otras opciones

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -3,6 +3,27 @@ from functools import reduce
 def doble(x):
     return x+x
 
+def sumatorioClasico(l):
+    resultado = 0
+    for valor in l:
+        resultado += valor
+    
+    return resultado
+
+def sumatorioDobleClasico(l):
+    resultado = 0
+    for valor in l:
+        resultado += valor*2
+    
+    return resultado
+
+def productoTotal(l):
+    resultado = 1
+    for valor in l:
+        resultado *= valor
+        
+    return resultado
+
 lista = [1, 3, -1, 15, 9]
 
 
@@ -25,11 +46,17 @@ listaPares1 = filter(esPar, lista)
 # a realizar según la función
 #la X representa el valor que va a devolver el reduce
 # la Y representa los valores de la lista.
+l= lista[:]
+#l es una copia de lista, un clon
+# si hacemos l=lista, no hacemos copia
+#l apuntaria a lista y si mofifico l, se modifica lista
+#haciendo lista[:] no pasa eso porque hago un clon.
 sumatorio = reduce(lambda x, y : x+y, lista)
 
 suma100 = reduce(lambda x,y: x+y, range(101))
 
 sumatorioDobles = reduce (lambda x,y: x+y*2, lista)
+#aquí sale 53 y no 54 porque reduce no suma el primer valor
 
 print(list(listaPares))
 print(list(listaPares1))
